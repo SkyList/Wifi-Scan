597f4ce3bd1e09381393f1e50c8bf73dae496f9c
SkyList <matheus.pantoja97@gmail.com>
2017-06-07 02:29:05 -0300

    .

--- wifi_Scan.py
+++ wifi_Scan.py
@@ -12,13 +12,14 @@ print (stdout_str)
 arq = open('res.txt', 'r');
 list = arq.readlines();
 
-ssid        = re.findall( r'(ESSID:["][A-z0-9_]*[^"])' , str(list) );
+ssid        = re.findall( r'(ESSID:["])([A-z0-9_]*[^"])*' , str(list) );
 address     = re.findall( r'(Address:) ([A-Z0-9][A-Z0-9]:[A-Z0-9][A-Z0-9]:[A-Z0-9][A-Z0-9]:[A-Z0-9][A-Z0-9]:[A-Z0-9][A-Z0-9]:[A-Z0-9][A-Z0-9])' , str(list) );
 quality     = re.findall( r'(Quality=[0-9][0-9])' , str(list) );
 level       = re.findall( r'(level=-[0-9][0-9])' , str(list) );
 channel     = re.findall( r'(Channel:[0-9][0-1]?)' , str(list) );
 frequency   = re.findall( r'(Frequency:[0-9]*.[0-9]?[0-9]?[0-9]?)' , str(list) );
 lastBeacon  = re.findall( r'(beacon: [0-9]*)' , str(list) );
+
 
 
 x=0
